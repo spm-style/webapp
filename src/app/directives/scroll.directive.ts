f16abc8015384efdcef2f2463a082eecaa1e9eb1
portable <portable@portables-MacBook-Pro.local>
2018-01-23 22:01:13 +0100

    add test if browser for scrollto and remove video.js replace by html 5 video native

--- src/app/directives/scroll.directive.ts
+++ src/app/directives/scroll.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Renderer2, OnInit, OnDestroy, HostListener, Inject } from '@angular/core'
+import { Directive, ElementRef, Renderer2, OnInit, OnDestroy, HostListener, Inject, PLATFORM_ID } from '@angular/core'
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { RDXAppState, select, Observable, Subscription } from '../store'
 import { DOCUMENT } from '@angular/platform-browser'
 
@@ -21,7 +22,8 @@ export class ScrollDirective implements OnInit, OnDestroy {
   constructor(
   	private _elementRef:ElementRef,
   	private _renderer:Renderer2,
-  	@Inject(DOCUMENT) private _document: any
+  	@Inject(DOCUMENT) private _document: any,
+		@Inject(PLATFORM_ID) private platformId: any
   ) { }
 
   ngOnInit(){
@@ -30,9 +32,11 @@ export class ScrollDirective implements OnInit, OnDestroy {
   		this._renderer.setStyle(this._elementRef.nativeElement, 'top', app.isMainContainerScrollable ? '' : -this._scroll.top + 'px')
   		this._renderer.setStyle(this._elementRef.nativeElement, 'position', app.isMainContainerScrollable ? '' : 'fixed')
   		if (app.isMainContainerScrollable) {
-		  	this._document.body
-        ? this._document.body.scrollTo(0, this._currentScrollBeforePopup)
-        : this._document.documentElement.scrollTo(0, this._currentScrollBeforePopup)
+				if (isPlatformBrowser(this.platformId)) {
+					this._document.body
+					? this._document.body.scrollTo(0, this._currentScrollBeforePopup)
+					: this._document.documentElement.scrollTo(0, this._currentScrollBeforePopup)
+        }
   		}
   	})
   }
@@ -43,10 +47,11 @@ export class ScrollDirective implements OnInit, OnDestroy {
 
   @HostListener('window:scroll', ['$event']) stopscroll($event){
    	this._scroll.event = $event
-   	this._scroll.top = $event.pageY
-			|| ($event.srcElement && $event.srcElement.documentElement && $event.srcElement.documentElement.scrollTop)
-			|| 0
+		// if (isPlatformBrowser(this.platformId)) {
+			this._scroll.top = $event.pageY
+				|| ($event.srcElement && $event.srcElement.documentElement && $event.srcElement.documentElement.scrollTop)
+				|| 0
+		// }
+
 	}
 }
-
-	
